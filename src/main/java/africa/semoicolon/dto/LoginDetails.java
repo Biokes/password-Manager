324465f3e9ffce86f1556365a452b81d81c7683d
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-03 18:42:43 +0100

    create Login details dto

---
+++ src/main/java/africa/semoicolon/dto/LoginDetails.java
@@ -0,0 +1,7 @@
+package africa.semoicolon.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDetails{
+}
--- src/main/java/africa/semoicolon/passwordmanagerRepository.java
+++ src/main/java/africa/semoicolon/passwordmanagerRepository.java
@@ -1,4 +1,7 @@
 package africa.semoicolon;
 
+import africa.semoicolon.dto.LoginDetails;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 public interface passwordmanagerRepository extends MongoRepository<LoginDetails, String >{
 }
