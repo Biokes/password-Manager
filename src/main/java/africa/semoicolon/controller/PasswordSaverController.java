6460b766457394548a570b2f4af585b7c8e935aa
Biokes <106444765+Biokes@users.noreply.github.com>
2024-04-06 07:23:37 +0100

    updating controller

--- src/main/java/africa/semoicolon/controller/PasswordSaverController.java
+++ src/main/java/africa/semoicolon/controller/PasswordSaverController.java
@@ -1,14 +1,12 @@
 package africa.semoicolon.controller;
 
-import africa.semoicolon.dtos.requests.RegisterRequest;
+import africa.semoicolon.dtos.reponses.LoginDetailsResponse;
+import africa.semoicolon.dtos.requests.*;
 import africa.semoicolon.exceptions.PasswordSaverException;
 import africa.semoicolon.services.PasswordManagerServices;
-import africa.semoicolon.services.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import static org.springframework.http.HttpStatus.*;
 
 @RestController
@@ -27,8 +25,58 @@ public class PasswordSaverController{
         }
     }
     @DeleteMapping("/Delete-Account")
-    public ResponseEntity<?> deleteAccount(){
+    public ResponseEntity<?> deleteAccount(@RequestBody LoginDetailsRequest loginDetails){
+        try{
+            userService.deleteUser(loginDetails);
+        }
+        catch(PasswordSaverException error){
+            return new ResponseEntity<>(error.getMessage(), BAD_REQUEST);
+        }
         return new ResponseEntity<>("",OK);
     }
-
+    @PatchMapping("/save-password")
+    public ResponseEntity<?> addPassword(@RequestBody SavePasswordRequest savePasswordRequest){
+        try{
+            userService.saveLoginDetails(savePasswordRequest);
+            return new ResponseEntity<>("Saved successfully", OK);
+        }catch(PasswordSaverException error){
+            return new ResponseEntity<>(error.getMessage(), BAD_REQUEST);
+        }
+    }
+    @PatchMapping("/update-website-password")
+    public ResponseEntity<?> updateWebsitePasssword(@RequestBody UpdateDetailsRequest updateDetailsRequest){
+        try{
+            userService.updateLoginDetails(updateDetailsRequest);
+        }catch( PasswordSaverException error ){
+            return new ResponseEntity<>(error.getMessage( ), BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Updated succefully", OK);
+    }
+    @GetMapping("/view-password-saved")
+    public ResponseEntity<?> viewPasswordDetails(@RequestBody ViewLoginDetailsRequest viewLoginDetailsRequest){
+        try{
+            LoginDetailsResponse response=userService.fetchDetails(viewLoginDetailsRequest);
+            return new ResponseEntity<>(String.format("%s : %s\n%s : %s\n%s : %s","Website name",response.getWebsiteName( ),
+                    "Website Username",response.getWebsiteUsername( ), "Website Password",response.getWebsitePasssword( )), OK);
+        }catch(PasswordSaverException error){
+            return new ResponseEntity<>(error.getMessage(), BAD_REQUEST);
+        }
+    }
+    @DeleteMapping("/delete-password-saved")
+    public ResponseEntity<?> deletePasswordDetails(@RequestBody DeleteWebsiteDetailsRequest detailsRequest){
+        try{
+        userService.deletePasswordDetails(detailsRequest);
+        return new ResponseEntity<>("deleted successfully", OK);
+        }
+        catch(PasswordSaverException error){
+            return new ResponseEntity<>(error.getMessage(), BAD_REQUEST);
+        }
+    }
+    @GetMapping("/view-all-passwords")
+    public ResponseEntity<?> viewAllPassword(){
+        try{}
+        catch(PasswordSaverException error){
+            return new ResponseEntity<>(error.getMessage(), BAD_REQUEST);
+        }
+    }
 }
